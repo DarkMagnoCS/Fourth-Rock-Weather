57920d23259b25a5f24cab543652bde45fd9aa5e
Dark <alejandro.magno.cs@gmail.com>
2024-09-23 17:15:26 -0300

    changing background, light and dark themes and updating information

--- mars-weather-hud/src/App.js
+++ mars-weather-hud/src/App.js
@@ -8,9 +8,25 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className="divider" />
       <main className="App-body">
-        <WeatherComponent />
+        <div className="weather-container">
+          <WeatherComponent />
+        </div>
+        <div className="info-container">
+          <h2>This Service Has Significant Missing Data</h2>
+          <p>
+            Please check out the seasonal weather report plot for an illustration of missing data and read this article about how dust and distance from the sun affect Insight's power situation.
+          </p>
+          <p>
+            NASA’s InSight Mars lander takes continuous weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’ equator. Please note that there are sometimes problems with the sensors on Mars that result in missing data!
+          </p>
+          <p>
+            This API provides per-Sol summary data for each of the last seven available Sols (Martian Days). As more data from a particular Sol are downlinked from the spacecraft, these values may change.
+          </p>
+          <p>
+            Check out more information at <a href="https://mars.nasa.gov/insight/weather/">this link</a>.
+          </p>
+        </div>
       </main>
       <Footer />
     </div>
