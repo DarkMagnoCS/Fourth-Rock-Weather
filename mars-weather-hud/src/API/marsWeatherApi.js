3abcc2549d9d7ab1eb71e7222be553c68f3cb697
Dark <alejandro.magno.cs@gmail.com>
2024-09-24 23:43:11 -0300

    fixing static page shown info

--- mars-weather-hud/src/API/marsWeatherApi.js
+++ mars-weather-hud/src/API/marsWeatherApi.js
@@ -1,4 +1,4 @@
-const API_KEY = 'DEMO_KEY';  // Replace with your API key
+const API_KEY = 'DEMO_KEY';
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
 async function getMarsWeatherData() {
--- mars-weather-hud/src/App.js
+++ mars-weather-hud/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <WeatherComponent />
         </div>
         <div className="info-container">
-          <h2>This Service Has Significant Missing Data</h2>
+          <h2>This Service Has Significant Missing Data due to Insight's lander power management</h2>
           <p>
             Please check out the seasonal weather report plot for an illustration of missing data and read this article about how dust and distance from the sun affect Insight's power situation.
           </p>
@@ -21,7 +21,7 @@ function App() {
             NASA’s InSight Mars lander takes continuous weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’ equator. Please note that there are sometimes problems with the sensors on Mars that result in missing data!
           </p>
           <p>
-            This API provides per-Sol summary data for each of the last seven available Sols (Martian Days). As more data from a particular Sol are downlinked from the spacecraft, these values may change.
+            This page shows the summary data for the last available Sol (Martian Days). As more data from a particular Sol are downlinked from the spacecraft, these values may change.
           </p>
           <p>
             Check out more information at <a href="https://mars.nasa.gov/insight/weather/">this link</a>.
